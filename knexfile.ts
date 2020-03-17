563334eb583d9378cdcd65b7864468b136431b30
Amanuel Teferi <aman.teferi.80@gmail.com>
2020-03-17 14:58:50 +0300

    fix connection

--- knexfile.ts
+++ knexfile.ts
@@ -3,13 +3,8 @@
 module.exports = {
 
   development: {
-    client: "postgresql",
-    connection: {
-      host: "localhost",
-      database: "my_db",
-      user: "manny",
-      password: "123456"
-    },
+    client: "pg",
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
@@ -20,13 +15,8 @@ module.exports = {
   },
 
   staging: {
-    client: "postgresql",
-    connection: {
-      host: "ec2-52-86-33-50.compute-1.amazonaws.com",
-      database: "dbm3mm62muhqc",
-      user: "rxcmwmienkfhfn",
-      password: "53d10be3ff0fbedb296af97057863efefa47d2c415d1e142301afc9e096edca8"
-    },
+    client: "pg",
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
