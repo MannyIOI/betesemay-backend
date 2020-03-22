e046f634b4d1b0d8dd3bc3ae9ca6f17a998b99a7
Amanuel Teferi <aman.teferi.80@gmail.com>
2020-03-22 22:50:49 +0300

    model renaming

--- src/api/employee/employee.model.ts
+++ src/api/employee/employee.model.ts
@@ -18,13 +18,13 @@ export class Employee extends unique(BaseModel) {
         return {
           type: "object",
           // firstname, lastname, email, phone number, address, role, photo
-          required: ["email", "phoneNumber", "firstName", "lastName", "jobTitle"],
+          required: ["email", "phone_number", "first_name", "last_name", "role"],
           properties: {
             email: { type: "string", minLength: 1, maxLength: 255 },
-            phoneNumber: { type: "string", minLength: 1, maxLength: 255 },
-            firstName: { type: "string", minLength: 1, maxLength: 255 },
-            lastName: { type: "string", minLength: 1, maxLength: 255 },
-            jobTitle: { type: "string", minLength: 1, maxLength: 255 }
+            phone_number: { type: "string", minLength: 1, maxLength: 255 },
+            first_name: { type: "string", minLength: 1, maxLength: 255 },
+            last_name: { type: "string", minLength: 1, maxLength: 255 },
+            role: { type: "string", minLength: 1, maxLength: 255 }
           }
         };
       }
